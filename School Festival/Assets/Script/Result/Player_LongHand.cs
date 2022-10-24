d80a9c25471ec5a526c1ea990a13e9ff9451a77e
Sugi <21cr0117@jec.ac.jp>
2022-10-25 01:09:14 +0900

    result：仮手の動き

---
+++ School Festival/Assets/Script/Result/Player_LongHand.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Player_LongHand : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    public Transform target2;
+
+    public float targetX;
+    public float targetY;
+
+    public float target2X;
+    public float target2Y;
+
+    public bool targetCheck;
+    void Start()
+    {
+        target.transform.position = new Vector2(targetX, targetY);
+
+        targetCheck = false;
+    }
+    
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed);
+
+        Invoke("Check", 3);
+
+        if(targetCheck == true)
+        {
+            target.transform.position = new Vector2(target2X, target2Y);
+
+            speed = 0.1f;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed);
+            targetCheck = true;
+        }
+    }
+
+    void Check()
+    {
+        targetCheck = true;
+    }
+}
