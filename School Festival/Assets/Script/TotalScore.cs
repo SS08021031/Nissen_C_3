7d6d0de143a348116e65563171bc6839bd11dfd3
Sugi <21cr0117@jec.ac.jp>
2022-10-25 17:53:10 +0900

    Ingame：食べ物のスコア修正

--- School Festival/Assets/Script/TotalScore.cs
+++ School Festival/Assets/Script/TotalScore.cs
@@ -11,48 +11,52 @@ public class TotalScore : MonoBehaviour
     public Text scoreText;
     public Text mathText;
     public static ResultRank rank = ResultRank.E;
-    
+
+    public bool isEnter;
     int score;
     
     // Start is called before the first frame update
     void Start()
     {
+        isEnter = false;
         // Invoke("sprite", 2);
 
-        rank = ResultRank.E;
-        score = Timer.getscore();
-        //Debug.Log(score);
-
-        mathText.text ="" + score;
-
-        if (score > 8000)
-
-            rank = ResultRank.A;
-
-
-
-        else if (score > 6000)
-
-            rank = ResultRank.B;
-
-
-        else if (score > 4000)
-
-            rank = ResultRank.C;
-
-
-        else if (score > 2000)
-
-            rank = ResultRank.D;
-
-
-        else if (score > 0)
         
             rank = ResultRank.E;
-            
-        
+            score = Timer.getscore();
+            //Debug.Log(score);
 
-        scoreText.text = "Result" + rank;
+            mathText.text = "" + score;
+
+            if (score > 8000)
+
+                rank = ResultRank.A;
+
+
+
+            else if (score > 6000)
+
+                rank = ResultRank.B;
+
+
+            else if (score > 4000)
+
+                rank = ResultRank.C;
+
+
+            else if (score > 2000)
+
+                rank = ResultRank.D;
+
+
+            else if (score > 0)
+
+                rank = ResultRank.E;
+
+
+
+            scoreText.text = "Result" + rank;
+        
     }
     void Sprite()
     {
@@ -62,12 +66,15 @@ public class TotalScore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Return))
+        if (isEnter == false)
         {
-            Nissensai.SendResult(rank);
+            if (Input.GetKey(KeyCode.Return))
+            {
+                isEnter = true;
+                Nissensai.SendResult(rank);
+            }
+
         }
-
-
     }
 
 
