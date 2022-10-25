8b6543f86603f5bf464ade9f10fc1ec6a70f2fd5
Sugi <21cr0117@jec.ac.jp>
2022-10-25 10:51:51 +0900

    result：スコア

--- School Festival/Assets/Script/TotalScore.cs
+++ School Festival/Assets/Script/TotalScore.cs
@@ -9,46 +9,49 @@ using Nissensai2022.Runtime;
 public class TotalScore : MonoBehaviour
 {
     public Text scoreText;
+    public Text mathText;
     public static ResultRank rank = ResultRank.E;
-
+    
     int score;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Invoke("sprite", 2);
 
         rank = ResultRank.E;
         score = Timer.getscore();
         //Debug.Log(score);
 
         if (score > 800)
-        
+
             rank = ResultRank.A;
-            
-        
+
+
 
         else if (score > 600)
-        
+
             rank = ResultRank.B;
-            
-        
+
+
         else if (score > 400)
-        
+
             rank = ResultRank.C;
-            
-        
+
+
         else if (score > 200)
-        
+
             rank = ResultRank.D;
         if (score > 0)
-        
+
             rank = ResultRank.E;
-            //ScoreE.instance.EScore();
-        
+       //ScoreE.instance.EScore();
+         mathText.text = "" + score;
         scoreText.text = "Result" + rank;
-
-
+    }
+    void Sprite()
+    {
+       
     }
 
     // Update is called once per frame
