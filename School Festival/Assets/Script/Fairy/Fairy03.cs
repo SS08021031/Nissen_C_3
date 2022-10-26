e51ebaca41d3466d2407b3777dd6f4e21955ee74
reehhe <21cr0131@jec.ac.jp>
2022-10-26 22:49:05 +0900

    妖精アニメーション

--- School Festival/Assets/Script/Fairy/Fairy03.cs
+++ School Festival/Assets/Script/Fairy/Fairy03.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Fairy03 : MonoBehaviour
 {
     public float speed = 10f;
-
+    
     public bool isReturn;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +29,9 @@ public class Fairy03 : MonoBehaviour
 
         if(isReturn == true)
         {
+            //transform.rotation = Quaternion.AngleAxis(180, new Vector3(0, 1, 0));
+            GameObject child = transform.GetChild(0).gameObject;
+            child.transform.rotation = Quaternion.AngleAxis(180, new Vector3(0, 1, 0));
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
 
